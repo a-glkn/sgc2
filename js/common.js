2462974d720778003beb58c92f520abb186d7c4a
Aleksandr Galkin <thebest-earnings@yandex.ru>
2022-01-28 17:32:29 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.menu-toggler').on('click', function(e) {
+		$('.mobile-menu, .mobile-menu__inner').height( $(window).height() - $('.header').height() );
+		
+		$('body').toggleClass('menu-openned');
+	});
+});
