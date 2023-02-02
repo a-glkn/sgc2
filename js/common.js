3f41c4c122e30f4096e7f9cf2f62c78ee1d30cc9
Alex <galkinamaria37@gmail.com>
2023-02-02 14:35:19 +0700

    food

--- js/common.js
+++ js/common.js
@@ -4,4 +4,30 @@ $(function() {
 		
 		$('body').toggleClass('menu-openned');
 	});
+
+	$('.tab-headers__item').on('click', function () { 
+
+		var $section = $(this).parents('section');
+		var i = $(this).index();
+
+
+		$('.tab-headers__item.active', $section).removeClass('active');
+		$(this).addClass('active');
+
+		$('.tab-contents__item.active', $section).removeClass('active');
+		$('.tab-contents__item:eq(' + i + ')', $section).addClass('active');
+
+	});
+
+
+
+	$('.food-slider').slick({
+		dots: true,
+		arrows: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		fade: true
+	});
+			
 });
